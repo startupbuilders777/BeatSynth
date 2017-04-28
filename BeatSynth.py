caf4e5ba3377ee00b94d0a2a54a89c97947801a1
Harman Singh <harmans@nulogy.com>
2017-04-28 14:20:34 -0400

    #fokasd

--- BeatSynth.py
+++ BeatSynth.py
@@ -1,5 +1,11 @@
 import BeatSynthUtility
+import librosa
+import librosa.display
+import IPython.display
+import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib.style as ms
 beat_names = ["Playboi Cari 1", "Playboi Carti 2"]
 beat_paths = ["Beats/beat1.mp3", "Beats/beat2.mp3"]
 
@@ -8,6 +14,11 @@ print(beats[1][0].shape)
 print(beats[0][0].shape)
 
 #BeatSynthUtility.plot_specgram(beat_names, beats)
-BeatSynthUtility.plot_waves(beat_names, beats)
+#BeatSynthUtility.plot_waves(beat_names, beats)
 #BeatSynthUtility.plot_log_power_specgram(beat_names, beats)
 
+plt.figure()
+librosa.display.waveplot(y=beats[1][0], sr=beats[1][1])
+librosa.display.waveplot(y=beats[0][0], sr=beats[0][1])
+
+plt.show()
