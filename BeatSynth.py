548cf97a9a989041df43f450a93e44a2781333ef
Harman Singh <harmans@nulogy.com>
2017-04-28 14:34:20 -0400

    #BARE REFACTORINDS DAWG

--- BeatSynth.py
+++ BeatSynth.py
@@ -10,15 +10,34 @@ beat_names = ["Playboi Cari 1", "Playboi Carti 2"]
 beat_paths = ["Beats/beat1.mp3", "Beats/beat2.mp3"]
 
 beats = BeatSynthUtility.load_sound_files(beat_paths)
-print(beats[1][0].shape)
+
 print(beats[0][0].shape)
+print(beats[1][0].shape)
+
 
 #BeatSynthUtility.plot_specgram(beat_names, beats)
 #BeatSynthUtility.plot_waves(beat_names, beats)
 #BeatSynthUtility.plot_log_power_specgram(beat_names, beats)
 
-plt.figure()
-librosa.display.waveplot(y=beats[1][0], sr=beats[1][1])
-librosa.display.waveplot(y=beats[0][0], sr=beats[0][1])
 
-plt.show()
+def displayWaveplot(y_arr, sr_arr):
+    for y, sr in zip(y_arr, sr_arr):
+        plt.figure()
+        librosa.display.waveplot(y=y, sr=sr)
+    plt.show()
+
+y_arr = []
+sr_arr = []
+
+for beat in beats:
+    y_arr.append(beat[0])
+    sr_arr.append(beat[1])
+
+
+displayWaveplot(y_arr, sr_arr)
+
+
+#plt.figure()
+#librosa.display.waveplot(y=beats[0][0], sr=beats[0][1])
+
+#plt.show()
